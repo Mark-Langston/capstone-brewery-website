af14ff85ce26a002efd424388ec47e9a29439040
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2026-03-03 19:47:31 -0800

    Update placeholder text for GitHub Actions v2

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 <h2>Welcome to Main Channel Brewing</h2>
 
-<p>Placeholder for Team Ironclad Solutions. Testing Auto-deploy to VPS GitHub Action</p>
+<p>Placeholder for Team Ironclad Solutions. Testing Auto-deploy to VPS GitHub Actions v2</p>
 
 <?php include 'footer.php'; ?>
