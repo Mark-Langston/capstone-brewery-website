55beb9f668ae845d748693b4e7d2de13cd179b47
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2026-03-10 15:46:42 -0700

    Testing image on index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <h2>Welcome to Main Channel Brewing</h2>
 
 <p>Placeholder for Team Ironclad Solutions. Testing Auto-deploy to VPS GitHub Actions Success? Testing a second time.</p>
-<br>
+
 <img src="assets/images/pages/Exterior.png" alt="Exterior of Brewery">
 
 <?php include 'footer.php'; ?>
