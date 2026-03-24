28a4d9291e6c665791b4c574a8569d6f2944fa97
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2026-03-24 16:26:12 -0700

    Remove smooth scrolling functionality
    Removed smooth scrolling script for anchor links.

--- index.php
+++ index.php
@@ -534,22 +534,4 @@ include 'header.php';
 </script>
 <?php endif; ?>
 
-
-<script>
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href');
-    const target = document.querySelector(targetId);
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth' });
-      setTimeout(() => {
-        history.replaceState(null, null, ' ');
-      }, 300);
-    }
-  });
-});
-</script>
-
-
 <?php include 'footer.php'; ?>
